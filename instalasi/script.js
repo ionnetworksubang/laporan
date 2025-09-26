f548315c39cefc9856c0a5b3a09b6371e7ddfdcd
doniariapurnama <doniariapurnama@gmail.com>
2025-09-26 12:34:25 +0700

    edit by doni

--- instalasi/script.js
+++ instalasi/script.js
@@ -40,9 +40,82 @@ const odpData = [
   "BCD","MAHKOTA 3","GG SERAM","DEDERUK","CISUGIH 3","CISUGIH 2","SELEDRI",
   "JL.DUKUH RAYA 2","NANAS","MAN 1","SUKALAKSANA 2","NGEBUL","AL MADANI","SYARIAH",
   "ODP PERDANA 2","WATERBOOM","ODP EPUL DEUI","RIMBUN","CONTER 2","DUSUN BABAKAN 3",
-  "BAPANG 5","LAPANG SIDODADI","RANGGADIPA"
-
-
+  "BAPANG 5","LAPANG SIDODADI","RANGGADIPA",
+  "SMJ","DODO","BAS","DT AQIQAH","OMLI","LAPANG CIBAROLA","OKE 2",
+  "VOLLY 2","ARAB","ANGGUR XIV","PERDANA","JAKSA 3","BABAKAN SUBANG 2",
+  "FLAMBOYAN 2","BLOK SAYUR 3","OFFICE SALES","GG MERAK 3","PUSAKA","GG LASMANA",
+  "CENDRAWASIH 3","PHILIPS 2","GG. BUNGUR 2","PANGLEJAR 5","GG ASEM",
+  "GG LIKA","SHOLEH","PALABUAN 2","YOSUDARSO","ABU","MAHKOTA 2",
+  "POS 2","KOPRI","LETNAN UKI 4","TIKUKUR","MANSU 2",
+  "ABIMANYU","	BIMA SAKTI 3","SRIKANDI 2","BTN CIERENG","NERAKO","KAPIRARAY 2","BOMBER",
+  "BLOK SAYUR 4","GG TONGKENG 2","GG PISANG","KANJENG MAMIH","RASIDI 2","SAKINA",
+  "PESONA REGENCY","NCEP","GG SUNDARSO 5","PAPUA","SISI CAI 3","SUNDARSO 4",
+  "GALATAMA 2","GG. ANGGUR 3","PASAR PUJASERA","AN NAJAH","LEMBAH CIMERTA",
+  "TOWER","KARTINI 9","DEPAN GREEN SUBANG","DAWEY","NYIMPLUNG","JUSSY 3",
+  "COSMOS","KARTINI 8","NENE UUR","CISUGIH","MEYER 2",
+  "MAYKHA","GG. HUNI","GG BUNGBULANG 3","ODP BLOK D 2","GG HANJUANG","GG TAWES 2","BLOK E",
+  "GG. BUAH 2","JL RA KARTINI 10","SKOTIS","FORTUN","BABAKAN UBAN 2","BABY JN",
+  "NEPTUNE","FINPROS","TCC","SIKEMBAR","AMBU","BEIJING",
+  "EDAN","BABAKAN CURUG","JL. RAYA TANJUNG WANGI","RENGKOLAN","FIRDAUS",
+  "BANG TOYIB","LANDEUH","JL. GIMCHEON","PALASARI KIDUL","PALASARI","CIHERANG 6",
+  "CIHERANG 5","CIHERANG 4","CIHERANG 3","SDN GUNUNG TUA","CIHERANG 2", "CIHERANG 1",
+  "PUNIK","PIGO","SD GUNUNGSARI","SMP GUNUNG TUA","BABAKAN PUTU","ANUWANI",
+  "PAMARIS 3","PAMARIS 2","PAMARIS 1","PITU","CIBAREGBEG 2",
+  "CIBAREGBEG 1","NUSANTARA","KUJANG","BABAKAN BAROKAH","DUSUN PANGKALAN 4","DUSUN PANGKALAN 3",
+  "DUSUN PANGKALAN 2","DUSUN PANGKALAN 1","SADANG 3","SADANG 2","SADANG 1", "UWA TERE",
+  "PONPES PAGELARAN","KARYA","GUNA UTAMA 2","GUNA UTAMA 1","NYINSO","CSN",
+  "UP","ATEP","	PARUNG 3","	PARUNG 2","	PARUNG",
+  "OKTA","UJUNG PARUNG","AL MUBAROKAH","YOBA","SD PARUNG","SLAY",
+  "DAMPIT","QOHAR","CIRANGKONG 1","HOLILUDIN","GONDRONG", "LAUK",
+  "SINDANGPALAY","TK PELITA","ODP UP 2","GEWOR","FLASHER 2","AL HUDA 2",
+  "INCUBETMEN 2","CARINGIN 2","CIHERANG 7","KHAYAL","ODP SORABI",
+  "ODP PARUNG 4","KAMAJAYA","ABAH","ALLULA","SCORPIO","CISUSUH",
+  "CIHERANG 12","	BALONG" ,"CIHERANG 11","CIHERANG 10","PONPES AL FAUZAN", "YANUSA",
+"IBTIDAIYAH","BAJAK","CIBAREGBEG 3","SPBU TAMBAK DAHAN","JL RAYA SIMPANG",
+  "CITRA","CIHERANG 9","CIHERANG 8","BLOK SAYUR 2","BLOK SAYUR 1",
+  "JL.SEROJA 1","JL.SEROJA 2","JL.SEROJA 3","JL.APEL RAYA 2","JL.APEL RAYA 1","GG.KARANG KENDAL 1",
+  "PLAMBOYAN","GG. PISANG","JL.APEL RAYA 3","JL.JAKSA 2","JL.SAWO 1","JL.DUKUH RAYA 1",
+  "JL.JAKSA 1","OTISTA","TUGU KOTA","GG TJ 1","MASJID NURUL HALIM 1","TARAKAN 1","BUMIAWI 1",
+  "SAGARA 1","JL.ASMARANDANA 1","LTK 1","JL.EMO 1","CIHEULEUT INDAH 1","JL.DIPONEGORO 1","	IDI CAHDI 1","GG.BUNGUR 1",
+  "GG.TONGKENG 1","GG.PALABUAN 1","JL.PANGLEJAR 1","TANJUNG SIANG 1","UKONG SUTAMADJA 1","PANGLEJAR 2","GG.SEJATI 1",
+  "GG.SUKAGALIH 1","JL.PEJUANG 2","MAYJEND SUTOYO 1","JL.PEJUANG 1","PHILIPS 1",
+  "GG SUKUN 1","MASJID NURUL HALIM 3","GG SUNDARSO 2","GG SUNDARSO 1","GG. ANGGUR 1","GG MERAK 1",
+  "TJOKRO 1","GG BUNGBULANG 1","GANESHA 1","TKIT 1","KANCIL 2","KANCIL 1",
+  "JUSSY 1","GG.BAPANG 1","JL.BASKET 1","PERMATA HIJAU 2","PERMATA HIJAU 1","BABAKAN UBAN","BLOK C",
+  "TAEKWANG","BLOK D","ABDUL GANI 1","GG.BUAH 1","JL.RADEN AJENG KARTINI 5	","JL.RADEN AJENG KARTINI 4","JL.RADEN AJENG KARTINI 3","JL.RADEN AJENG KARTINI 2","JL.RADEN AJENG KARTINI 1","	KIARA 3",
+  "GG.KIARA 2","GG.KIARA 1","POS 1",
+  "SAWAH 1","MASJID NURUL HALIM 2","KINTAN 2","BENTENG 1","CIBAROLA 1",
+  "KINTAN 1","LETNAN UKI 1","RA KARTINI 6","BAPANG 3","DUSUN BABAKAN 1","KP CONTO 1",
+  "BAPANG 2","UNSUB 1","BWI 1","LETNAN UKI 2","KINTAN 3","JL METEOR 1",
+  "KASERBA 1","PASIR KAREUMBI 1","JL. BILYAR 1","JL. VOLLY 1","JL. GOLF 1","DISDUKCAPIL","MYGO",
+  "SMAN 4 SUBANG","BUMI ABDI PRAJA 3","BUMI ABDI PRAJA 1","BUMI ABDI PRAJA 2","	GG. ANGGUR 2","LTK2","GG KARTANINGSIH","CIKAWALI 2","CIKAWALI",
+  "MANSU","CIKALAPA","SISI CAI","BSR 5",
+  "BSR 3","BSR 4","BSR 2","BSR 1","KARTINI 7",
+  "BAROKAH","MEYER","ALUN ALUN","BULANSARI","PURNAMA","MALABAR",
+  "GALATAMA","TUJI","KINTAN 5","JAPATI","GG CIUNG","GREEN SUBANG 1",
+  "MAHKOTA 1","CONTER","KINTAN 4","BANGBANG KACA","JL. SOMPI","GG. BIDARA","GG PANGRANGO",
+  "DARMAKUSUMAH","CANGAK","TANGKUBAN PERAHU","GG. ANGGREK","JL. RAYA SUKAMELANG 2","JL. RAYA SUKAMELANG","GG. SALUYU 2","GG. MAWAR",
+"GG. SALUYU","JL.PEJUANG 3","GG CEREME","JAMBU SBG",
+  "RANGGAWULUNG","ANGGUR RAYA","TERMINAL 2","TERMINAL 1","ASAHI",
+  "SUKALAKSANA","SUKAMAJU","JAYASENA","GG. CUAN","AL MU'MIN","JL. PANJI",
+  "NOTO","SRIKANDI","SUKAWARNA","JETU","GG RASIDI 1","JUSSY 2",
+  "DANGDEUR","GG UYUH 2","KELAPA RESIDENCE","GG UYUH","SADEWA","BIMA SAKTI 1","BIMA SAKTI 2",
+  "GPS","GELORA","YONKO","QYANDA",
+  "KENANGA","OKE","PANGKALAN","DUSUN BABAKAN 2","LETNAN UKI 3",
+  "DELTA","BUMI KARTINI 2","BUMI KARTINI","PERMATA HIJAU 3","CIERENG","HENING",
+  "DAWUAN","SITU SAEUR","BATANG KAPEH","GG. TARUNA","JL. PRAMUKA 3","JL. PRAMUKA 2",
+  "JL. PRAMUKA 1","FLASHER","CARINGIN","AMANAH 2","AL HUDA","WARUNG KADU","GG. INCUBETMEN",
+ "AMANAH","GG SUNDARSO 3","GG MERAK 2","SAGARA 2",
+  "SISI CAI 2","LASKAR INDONESIA","DJUNG","BAPANG 4","KANCIL 3",
+  "KINTAN 6","SAWARGI","ODP GG FILTER","ODP HIJAU","PONDOK INDAH","PALUGADA",
+  "LANGENSARI","POLITEKNIK","KAPIRARAY","PARIKESIT","AS SALAM","RAFA",
+  "KANAYA","SITRUN","GG. MELATI","CENDERAWASIH 2","KOISIWA","GG SUKUN 2","ATTAARUF",
+  "PERUMNAS","GG. KITRI","TERANG","GG LEWO",
+  "SUKARAHAYU","POLARIS","JERUK","KAFFA","GG BUNGBULANG 2",
+  "BELITUNG","LAPANG SAMPEU","ALFOUR","ATREA","AKIMORA","JACE",
+  "KIARA 5","GREEN SUBANG 2","GG.KIARA 4","KHAER","BAPE","PAHER",
+  "BSR 7","BSR 6","KABOA","BANG KOMAR","PANGHEGAR","PAGADEN 1","PAGADEN 2",
+  "PAGADEN 3","PAGADEN 4","PEJUANG DALAM","POS 3",
     ];
 
     // Fungsi untuk update tanggal (hanya hari, tanggal, bulan, tahun)
@@ -88,16 +161,16 @@ const odpData = [
         </div>
         <input type="hidden" id="jenis{id}" value="Instalasi">
         
-        <label for="nama{id}">Nama :</label>
+        <label for="nama{id}">Nama</label>
         <input type="text" id="nama{id}" placeholder="Nama pelanggan">
         
-        <label for="cid{id}">CID :</label>
+        <label for="cid{id}">CID</label>
         <input type="text" id="cid{id}" placeholder="Nomor CID">
         
-        <label for="email{id}">Email :</label>
+        <label for="email{id}">Email</label>
         <input type="email" id="email{id}" placeholder="Email pelanggan">
         
-        <label for="olt{id}">OLT :</label>
+        <label for="olt{id}">OLT</label>
         <select id="olt{id}">
           <option value="">-- Pilih OLT --</option>
           <option value="OFFICE SUBANG RAISECOM">OFFICE SUBANG RAISECOM</option>
@@ -112,12 +185,12 @@ const odpData = [
         
         <div class="form-grid">
           <div class="search-container">
-            <label for="odp{id}">ODP :</label>
+            <label for="odp{id}">ODP</label>
             <input type="text" id="odp{id}" placeholder="Ketik untuk mencari ODP" oninput="searchODP(this, 'odpResults{id}')">
             <div class="search-results" id="odpResults{id}"></div>
           </div>
           <div>
-            <label for="port{id}">Port :</label>
+            <label for="port{id}">Port</label>
             <select id="port{id}">
               <option value="">-- Pilih Port --</option>
               <option value="1">1</option>
@@ -140,18 +213,18 @@ const odpData = [
           </div>
         </div>
         
-<label for="jenis_perangkat{id}">Jenis Perangkat :</label>
+<label for="jenis_perangkat{id}">Jenis Perangkat</label>
 <select id="jenis_perangkat{id}" onchange="handleDeviceChange(this, {id})">
   <option value="XPON ONU" selected>XPON ONU</option>
   <option value="RISECOME">RISECOME</option>
 </select>
 
-<label for="sn_perangkat{id}">SN Perangkat:</label>
+<label for="sn_perangkat{id}">SN Perangkat</label>
 <input type="text" id="sn_perangkat{id}" value="DF1D-" placeholder="Serial number perangkat">
 
         <div class="form-grid">
           <div>
-            <label for="dropcore{id}">Dropcore :</label>
+            <label for="dropcore{id}">Dropcore</label>
             <select id="dropcore{id}">
               <option value="">Pilih panjang</option>
               <option value="50 Meter">50 Meter</option>
@@ -161,7 +234,7 @@ const odpData = [
             </select>
           </div>
           <div>
-            <label for="patchcord{id}">Patchcord :</label>
+            <label for="patchcord{id}">Patchcord</label>
             <select id="patchcord{id}">
               <option value="">Pilih jumlah</option>
               <option value="1 Pcs" selected>1 Pcs</option>
@@ -171,7 +244,7 @@ const odpData = [
           </div>
         </div>
         
-        <label for="keterangan{id}">Keterangan :</label>
+        <label for="keterangan{id}">Keterangan</label>
         <input type="text" id="keterangan{id}" value="Selesai">
       `,
       
@@ -258,10 +331,22 @@ const odpData = [
         </div>
 
         <label for="ken{id}">Kendala</label>
-        <input type="text" id="ken{id}" placeholder="Masalah yang ditemukan">
+        <select id="ken{id}">
+          <option value="">Pilih Kendala</option>
+          <option value="Pelanggan tidak ada di tempat">Pelanggan tidak ada di tempat</option>
+          <option value="Kendala teknis">Kendala teknis</option>
+          <option value="Cuaca buruk">Cuaca buruk</option>
+          <option value="Lainnya">Lainnya</option>
+        </select>
 
         <label for="tin{id}">Tindakan</label>
-        <input type="text" id="tin{id}" placeholder="Tindakan perbaikan">
+        <select id="tin{id}">
+          <option value="">Pilih Tindakan</option>
+          <option value="Pelanggan tidak ada di tempat">Pelanggan tidak ada di tempat</option>
+          <option value="Kendala teknis">Kendala teknis</option>
+          <option value="Cuaca buruk">Cuaca buruk</option>
+          <option value="Lainnya">Lainnya</option>
+        </select>
 
         <div class="form-grid">
           <div>
@@ -279,9 +364,14 @@ const odpData = [
             <label for="dropcore{id}">Dropcore</label>
             <input type="text" id="dropcore{id}" placeholder="Panjang dropcore">
           </div>
-          <div>
-            <label for="patchcord{id}">Patchcord</label>
-            <input type="text" id="patchord{id}" placeholder="Panjang patchord">
+                    <div>
+            <label for="patchcord{id}">Patchcord :</label>
+            <select id="patchcord{id}">
+              <option value="">Pilih jumlah</option>
+              <option value="1 Pcs" selected>1 Pcs</option>
+              <option value="2 Pcs">2 Pcs</option>
+              <option value="3 Pcs">3 Pcs</option>
+            </select>
           </div>
         </div>
 
@@ -353,7 +443,7 @@ function handleDeviceChange(selectElement, id) {
       div.id = "form" + formCount;
 
       let formHTML = formTemplates[type].replace(/{id}/g, formCount);
-      formHTML += `<button class="delete" onclick="deleteForm(${formCount})">x</button>`;
+      formHTML += `<button class="delete" onclick="deleteForm(${formCount})">Hapus</button>`;
       
       div.innerHTML = formHTML;
       document.getElementById("forms").appendChild(div);
@@ -416,7 +506,7 @@ function handleDeviceChange(selectElement, id) {
       let nomorMaintenance = 1;
 
       if (instalasiData.length > 0) {
-        laporan += `INSTALASI BARU:\n`;
+        laporan += `INSTALASI:\n`;
         instalasiData.forEach(data => {
           let i = data.id;
           let cid = document.getElementById("cid"+i)?.value || '';
@@ -439,7 +529,7 @@ function handleDeviceChange(selectElement, id) {
       }
 
       if (rescheduleData.length > 0) {
-        laporan += `Reschedule:\n`;
+        laporan += `RESCHEDULE:\n`;
         rescheduleData.forEach(data => {
           let i = data.id;
           let cid = document.getElementById("cid"+i)?.value || '';
@@ -478,21 +568,7 @@ function handleDeviceChange(selectElement, id) {
             nomorMaintenance++;
           }
         });
-        function validateForm(id) {
-  const cid = document.getElementById(`cid${id}`).value.trim();
-  const nama = document.getElementById(`nama${id}`).value.trim();
-  const odp = document.getElementById(`odp${id}`).value.trim();
-  const kendala = document.getElementById(`ken${id}`).value.trim();
-  const tindakan = document.getElementById(`tin${id}`).value.trim();
-
-  if (!cid || !nama || !odp || !kendala || !tindakan) {
-    alert("Harap isi CID, Nama, ODP, Kendala, dan Tindakan!");
-    return false;
-  }
-
-  return true; // port, dropcore, patchcord boleh kosong
-}
-
+        
       }
 
       document.getElementById("output").innerText = laporan.trim();
@@ -513,7 +589,7 @@ function handleDeviceChange(selectElement, id) {
         document.getElementById("forms").innerHTML = "";
         document.getElementById("output").innerText = "";
         document.getElementById("teamName").value = "";
-        document.getElementById("reportTitle").value = "LAPORAN  SUBANG";
+        document.getElementById("reportTitle").value = "UPDATE LAPORAN INSTALASI SUBANG*";
         document.getElementById("reportDate").value = "";
         formCount = 0;
         updateDate();
